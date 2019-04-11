64f93eb925e96f6f12df583b8af38af361f62f37
Jatan Mehta <22585732+uncertainquark@users.noreply.github.com>
2019-04-11 16:06:38 +0530

    Update elementary-os-5-juno.sh

--- elementary-os-5-juno.sh
+++ elementary-os-5-juno.sh
@@ -4,33 +4,27 @@
 sudo apt -y install xserver-xorg-input-synaptics
 
 # Remove stuff
-sudo apt -y autoremove io.elementary.camera epiphany-browser pantheon-mail noise
+sudo apt -y autoremove io.elementary.camera epiphany-browser noise pantheon-calculator pantheon-mail
 
 # Minimal browser installs
 sudo apt -y install firefox chromium-browser --no-install-recommends
 
 # Install some apps from Ubuntu repos
-sudo apt -y install gnome-disk-utility gnome-usage deepin-picker mpv git
+sudo apt -y install gnome-disk-utility gnome-software gnome-calculator deepin-picker mpv git ttf-mscorefonts-installer
 
 # Install AppCenter apps
-sudo apt -y install com.github.donadigo.eddy com.github.davidmhewitt.torrential com.github.davidmhewitt.clipped com.github.donadigo.appeditor com.github.peteruithoven.resizer
+sudo apt -y install com.github.davidmhewitt.torrential com.github.davidmhewitt.clipped com.github.donadigo.appeditor com.github.peteruithoven.resizer
 
 # Enable PPAs
 sudo apt -y install software-properties-gtk --no-install-recommends
-
-# LibreOffice done right
-sudo add-apt-repository -y ppa:libreoffice/ppa
-sudo apt -y install libreoffice ttf-mscorefonts-installer
 
 # Install Elementary tweaks
 sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
 sudo apt -y install elementary-tweaks
 
-# Get updated versions of Geary mail client
-sudo add-apt-repository ppa:geary-team/releases
-sudo apt install geary
-
-# Update and Upgrade the packages
+# Cleanups and upgrades
+sudo apt autoremove firefox-locale-* language-pack-* xul-ext-ubufox
+sudo apt install firefox-locale-en language-pack-en language-pack-gnome-en
 sudo apt -y update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean
 
 # Install Dark dock theme
@@ -39,3 +33,12 @@ cd frost-plank-theme
 ./install.sh
 cd ..
 rmdir frost-plank-theme
+
+# Install Flatpak apps
+sudo add-apt-repository ppa:alexlarsson/flatpak
+sudo apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+Reboot the system
+
+# Install Snap apps
+telegram-desktop
